25d19982063ed9f6d8e2cf48d40b67e11df7b4f4
Anesto27 <anes.2503@gmx.at>
2019-02-08 20:48:00 +0100

    add jquery

--- Javascript/myscript.js
+++ Javascript/myscript.js
@@ -1,4 +1,4 @@
-for (i=0; i<selcetion.length; i++){
+for (i=0; i<=selection.[1]; i++){
 	$(".content").append("<div><div class='box1'><img src='"+selection[i].img+"'<div class='box2'><h1>"+selection[i].movieName+"</h1>"+'<br>'+"<p>"+selection[i].description+"</p><div class='box3'><p></p><div class='box4'></div></div></div></div>");
 
 	$(".content > div:eq("+(i)+")").addClass("box");
